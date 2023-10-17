f23038babc01967333aae2bb701fc32f1188c042
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2023-10-17 06:19:21 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+// We are human. Sometimes we need same work again and again. If we reduce our Work we can use loop. Suppose we count 1-100 and keep addition of this value. i think is not a hard work but on the otherhand if we count 1-1000 and keep addition of this all value. This time it is very hard for us. This time loop can make our life easy. Here i elaborate about loop and loop all of these things.
+
+// // For loop structure
+// for(initialization, condition, increment/decrement){
+//     // code write here
+//   }
+
+// This is the Simple For Loop
+
+// for(let a=0;a<=100;a++){
+//     console.log(a)
+// }
+
+// anothet for loop example
+// for(let a=0;a<=100;a++){
+//     console.log(`${a}*${a}=${a * a}`)
+// }
+
+// output will be
+// 0 * 0 = 0
+// 1 * 1 = 1
+// 2 * 2 = 4
+// 3 * 3 = 9
+// 4 * 4 = 16
+// 5 * 5 = 25
+//.................. It will be continue as like to 100.
+
+//......Adding all elements in the array......
+
+// const numbers=[1,2,3,4,5,7]
+
+// let sum=0
+
+// for(let a=0;a<numbers.length;a++){
+//     sum=sum+numbers[a]
+// }
+// console.log(sum)
+
+// output will be 22
+
+
+//......Creating a new array based on the existing array......
+
+// const numbers=[1,2,3,4]
+
+// let newArry=[];
+
+// for(let i=0;i<numbers.length;i++){
+//     newArry.push(numbers[i]**2)
+// }
+
+
+// console.log(newArry)
+
+// output will be [1,4,9,16]
+
+// ...While Loop
+
+// let i=0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// output 0,1,2,3,4,5
+
+//.....Do while Loop
+
+// let i=0;
+
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i<=5)
+
+// output 0,1,2,3,4,5
+
+// Break statement in Loop
+
+// for (let i=0;i<=5;i++){
+//    if(i==3){
+//     break
+//    }
+//    console.log(i)
+// }
+
+
+
